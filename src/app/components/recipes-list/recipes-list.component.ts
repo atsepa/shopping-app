1acb4e9e97a235f0d4fb8c5ed6d84c4626229222
Francisco Garcia <beickon@franciscos-mbp.myfiosgateway.com>
2020-03-07 20:36:16 -0500

    Add all basic logic to components

--- src/app/components/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes-list/recipes-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../../models/recipes.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+  recipies : Recipe[] = [
+    new Recipe('Test','This is a test','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg')
+  ]
 
   constructor() { }
 
